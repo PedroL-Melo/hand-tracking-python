0e204168cbc50f696f0825be7391c3cd22a80364
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-25 22:02:37 -0500

    finalizando o código

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ resultados_ia = None
 model_path = r'C:\Users\pedro\OneDrive\Documentos\handtracking\gesture_recognizer.task'
 base_options = BaseOptions(model_asset_path=model_path)
 
-# Create a gesture recognizer instance with the live stream mode:
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
     print('gesture recognition result: {}'.format(result))
     global resultados_ia
@@ -36,15 +35,9 @@ with GestureRecognizer.create_from_options(options) as recognizer:
                     pixel_x = int(ponto.x * largura)
                     pixel_y = int(ponto.y * altura)
                     cv2.circle(frame, (pixel_x, pixel_y), 5, (0, 255, 0), -1)
-                
         cv2.imshow('Rastreador', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'): break
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
-        # Send live image data to perform gesture recognition.
-        # The results are accessible via the `result_callback` provided in
-        # the `GestureRecognizerOptions` object.
-        # The gesture recognizer must be created with the live stream mode.
         recognizer.recognize_async(mp_image, timestamp)
     camera.release()
     cv2.destroyAllWindows()
-    
